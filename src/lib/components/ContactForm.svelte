85beaa1b4f87fbb84877e0da5d3d4855359115be
JPTrey <jptrey91@gmail.com>
2024-07-15 17:19:40 -0400

    Update ContactForm, add env example.

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -1,5 +1,5 @@
 <script>
-	const FORM_ACCESS_KEY = '';
+	const FORM_ACCESS_KEY = import.meta.env.FORM_ACCESS_KEY;
 	let form;
 	let result;
 
@@ -40,17 +40,105 @@
 	}
 </script>
 
-<form method="POST" id="form" bind:this={form} on:submit={onSubmit}>
+<form class="max-w-md mx-auto mt-10" method="POST" id="form" bind:this={form} on:submit={onSubmit}>
 	<input type="hidden" name="access_key" value={FORM_ACCESS_KEY} />
-	<input type="hidden" name="subject" value="New Ban Appeal" />
+	<input type="hidden" name="subject" value="New Inquery" />
 	<input type="checkbox" name="botcheck" id="" style="display: none;" />
 
-	<!-- Custom Form Data -->
-	<input type="text" name="username" placeholder="Discord Username" required />
-	<input type="text" name="reason" placeholder="Official Reason You Were Banned" required />
-	<textarea name="message" placeholder="Your appeal..." required></textarea>
+	<div class="relative z-0 w-full mb-5 group">
+		<input
+			type="email"
+			name="floating_email"
+			id="floating_email"
+			class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+			placeholder=" "
+			required
+		/>
+		<label
+			for="floating_email"
+			class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+			>Email address</label
+		>
+	</div>
+	<div class="grid md:grid-cols-2 md:gap-6">
+		<div class="relative z-0 w-full mb-5 group">
+			<input
+				type="text"
+				name="floating_first_name"
+				id="floating_first_name"
+				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+				placeholder=" "
+				required
+			/>
+			<label
+				for="floating_first_name"
+				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+				>First name</label
+			>
+		</div>
+		<div class="relative z-0 w-full mb-5 group">
+			<input
+				type="text"
+				name="floating_last_name"
+				id="floating_last_name"
+				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+				placeholder=" "
+				required
+			/>
+			<label
+				for="floating_last_name"
+				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+				>Last name</label
+			>
+		</div>
+	</div>
+	<div class="grid md:grid-cols-2 md:gap-6">
+		<div class="relative z-0 w-full mb-5 group">
+			<input
+				type="tel"
+				pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+				name="floating_phone"
+				id="floating_phone"
+				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+				placeholder=" "
+				required
+			/>
+			<label
+				for="floating_phone"
+				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+				>Phone number</label
+			>
+		</div>
+		<div class="relative z-0 w-full mb-5 group">
+			<input
+				type="text"
+				name="floating_company"
+				id="floating_company"
+				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+				placeholder=" "
+			/>
+			<label
+				for="floating_company"
+				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+				>Company (optional)</label
+			>
+		</div>
+	</div>
 
-	<button type="submit">Submit</button>
-
+	<div class="relative z-0 w-full mb-5 group">
+		<div class="relative z-0 w-full mb-5 group">
+			<textarea
+				id="message"
+				rows="4"
+				class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="Your Message..."
+			></textarea>
+		</div>
+	</div>
+	<button
+		type="submit"
+		class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+		>Submit</button
+	>
 	<div id="result" bind:this={result}></div>
 </form>
