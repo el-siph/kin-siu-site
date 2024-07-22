4a51c7d8341863aa8cf900dc3c7dcf98d65292f1
JPTrey <jptrey91@gmail.com>
2024-07-22 13:41:50 -0400

    Improve formatting, functionality of Contact form

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -1,5 +1,5 @@
 <script>
-	const FORM_ACCESS_KEY = import.meta.env.FORM_ACCESS_KEY;
+	const FORM_ACCESS_KEY = import.meta.env.VITE_FORM_ACCESS_KEY;
 	let form;
 	let result;
 
@@ -38,9 +38,62 @@
 				}, 3000);
 			});
 	}
+
+	const isNumericInput = (event) => {
+		const key = event.keyCode;
+		return (
+			(key >= 48 && key <= 57) || // Allow number line
+			(key >= 96 && key <= 105) // Allow number pad
+		);
+	};
+
+	const isModifierKey = (event) => {
+		const key = event.keyCode;
+		return (
+			event.shiftKey === true ||
+			key === 35 ||
+			key === 36 || // Allow Shift, Home, End
+			key === 8 ||
+			key === 9 ||
+			key === 13 ||
+			key === 46 || // Allow Backspace, Tab, Enter, Delete
+			(key > 36 && key < 41) || // Allow left, up, right, down
+			// Allow Ctrl/Command + A,C,V,X,Z
+			((event.ctrlKey === true || event.metaKey === true) &&
+				(key === 65 || key === 67 || key === 86 || key === 88 || key === 90))
+		);
+	};
+
+	const enforceFormat = (event) => {
+		// Input must be of a valid number format or a modifier key, and not longer than ten digits
+		if (!isNumericInput(event) && !isModifierKey(event)) {
+			event.preventDefault();
+		}
+	};
+
+	const formatToPhone = (event) => {
+		if (isModifierKey(event)) {
+			return;
+		}
+
+		const input = event.target.value.replace(/\D/g, '').substring(0, 10); // First ten digits of input only
+		const areaCode = input.substring(0, 3);
+		const middle = input.substring(3, 6);
+		const last = input.substring(6, 10);
+
+		if (input.length > 6) {
+			event.target.value = `(${areaCode}) ${middle} - ${last}`;
+		} else if (input.length > 3) {
+			event.target.value = `(${areaCode}) ${middle}`;
+		} else if (input.length > 0) {
+			event.target.value = `(${areaCode}`;
+		}
+	};
+
+	let inputElement;
 </script>
 
-<form class="max-w-md mx-auto mt-10" method="POST" id="form" bind:this={form} on:submit={onSubmit}>
+<form class="max-w-2xl mx-auto mt-10" method="POST" id="form" bind:this={form} on:submit={onSubmit}>
 	<input type="hidden" name="access_key" value={FORM_ACCESS_KEY} />
 	<input type="hidden" name="subject" value="New Inquery" />
 	<input type="checkbox" name="botcheck" id="" style="display: none;" />
@@ -48,7 +101,7 @@
 	<div class="relative z-0 w-full mb-5 group">
 		<input
 			type="email"
-			name="floating_email"
+			name="Email address"
 			id="floating_email"
 			class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 			placeholder=" "
@@ -64,7 +117,7 @@
 		<div class="relative z-0 w-full mb-5 group">
 			<input
 				type="text"
-				name="floating_first_name"
+				name="First name"
 				id="floating_first_name"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
@@ -79,7 +132,7 @@
 		<div class="relative z-0 w-full mb-5 group">
 			<input
 				type="text"
-				name="floating_last_name"
+				name="Last name"
 				id="floating_last_name"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
@@ -95,9 +148,11 @@
 	<div class="grid md:grid-cols-2 md:gap-6">
 		<div class="relative z-0 w-full mb-5 group">
 			<input
+				bind:this={inputElement}
+				on:keydown={enforceFormat}
+				on:keyup={formatToPhone}
 				type="tel"
-				pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-				name="floating_phone"
+				name="Phone number"
 				id="floating_phone"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
@@ -112,7 +167,7 @@
 		<div class="relative z-0 w-full mb-5 group">
 			<input
 				type="text"
-				name="floating_company"
+				name="Company name"
 				id="floating_company"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
@@ -130,6 +185,7 @@
 			<textarea
 				id="message"
 				rows="4"
+				name="Message"
 				class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 				placeholder="Your Message..."
 			></textarea>
